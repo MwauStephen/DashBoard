687178648a4022871aee9a64c716c6cba20141f1
Mwau stephen <stephenmwau41@gmail.com>
2022-07-01 14:11:21 +0800

    added the components routing functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import Home from "./Components/Pages/HomePage/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Pages/LoginPage/Login";
+import List from "./Components/Pages/ListPage/List";
+import SinglePage from "./Components/Pages/SinglePage/SinglePage";
+import NewPage from "./Components/Pages/NewPage/NewPage";
 function App() {
-  return <div>Welcome to react</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="list">
+              <Route index element={<List />} />
+              <Route path=":ListUsers" element={<SinglePage />} />
+            </Route>
+            <Route>
+              <Route index element={<List />} />
+              <Route path=":productId" element={<SinglePage />} />
+              <Route path="new" element={<NewPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
