ac0f8d02037fd8db6e87d620f612c4c4887f7034
Mwau stephen <stephenmwau41@gmail.com>
2022-07-03 16:03:53 +0800

    finished the navigation bar component

--- src/Components/navBar/NavBar.js
+++ src/Components/navBar/NavBar.js
@@ -5,6 +5,8 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import { Badge, Avatar } from "@mui/material";
+
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import styles from "./NavBar.module.css";
 
@@ -14,25 +16,39 @@ const NavBar = () => {
       <div className={styles.wrapper}>
         <div className={styles.search}>
           <input type="text" placeholder="search...." />
-          <SearchIcon />
+          <SearchIcon className={styles.icon} />
         </div>
-        <div className={styles.item}>
-          <LanguageIcon />
-        </div>
-        <div className={styles.item}>
-          <DarkModeOutlinedIcon />
-        </div>
-        <div className={styles.item}>
-          <FullscreenExitIcon />
-        </div>
-        <div className={styles.item}>
-          <NotificationsNoneOutlinedIcon />
-        </div>
-        <div className={styles.item}>
-          <ChatBubbleOutlineOutlinedIcon />
-        </div>
-        <div className={styles.item}>
-          <ListOutlinedIcon />
+        <div className={styles.items}>
+          <div className={styles.item}>
+            <LanguageIcon className={styles.icon} />
+            English
+          </div>
+          <div className={styles.item}>
+            <DarkModeOutlinedIcon className={styles.icon} />
+          </div>
+          <div className={styles.item}>
+            <FullscreenExitIcon className={styles.icon} />
+          </div>
+          <div className={styles.item}>
+            <Badge color="warning" badgeContent={1}>
+              <NotificationsNoneOutlinedIcon className={styles.icon} />
+            </Badge>
+          </div>
+          <div className={styles.item}>
+            <Badge color="warning" badgeContent={1}>
+              <ChatBubbleOutlineOutlinedIcon className={styles.icon} />
+            </Badge>
+          </div>
+          <div className={styles.item}>
+            <ListOutlinedIcon className={styles.icon} />
+          </div>
+          <div className={styles.item}>
+            <Avatar
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: "2.4rem", height: "2.4rem " }}
+            />
+          </div>
         </div>
       </div>
     </div>
