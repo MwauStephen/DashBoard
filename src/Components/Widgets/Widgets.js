e2def5c881cd841fc0c8362f1e558dc9b95c71f1
Mwau stephen <stephenmwau41@gmail.com>
2022-07-03 18:25:41 +0800

    finished the widget section

--- src/Components/Widgets/Widgets.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Widgets.module.css";
-
-const Widgets = () => {
-  return <div className={styles.widget}>Widgets</div>;
-};
-
-export default Widgets;
