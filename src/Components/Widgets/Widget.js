2ff0a1539448d24bb0deb880216bec788f3e4d96
Mwau stephen <stephenmwau41@gmail.com>
2022-07-03 18:25:05 +0800

    added the widgets content dynamically

---
+++ src/Components/Widgets/Widget.js
@@ -0,0 +1,72 @@
+import React from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import styles from "./Widget.module.css";
+
+const Widget = (props) => {
+  let data;
+  const amount = 100;
+  const percentage = 20;
+
+  switch (props.type) {
+    case "users":
+      data = {
+        title: "users",
+        hasMoney: false,
+        link: "view users",
+        icon: <PersonOutlineOutlinedIcon className={styles.icon} />,
+      };
+      break;
+    case "orders":
+      data = {
+        title: "orders",
+        hasMoney: false,
+        link: "view orders",
+        icon: <ShoppingCartOutlinedIcon className={styles.icon} />,
+      };
+      break;
+    case "earnings":
+      data = {
+        title: "earnings",
+        hasMoney: true,
+        link: "view  earnings",
+        icon: <MonetizationOnOutlinedIcon className={styles.icon} />,
+      };
+      break;
+    case "balance":
+      data = {
+        title: "balance",
+        hasMoney: true,
+        link: "view balance",
+        icon: <AccountBalanceOutlinedIcon className={styles.icon} />,
+      };
+      break;
+
+    default:
+      break;
+  }
+  return (
+    <div className={styles.widget}>
+      <div className={styles.left}>
+        <span className={styles.title}>{data.title}</span>
+        <span className={styles.counter}>
+          {data.hasMoney ? `${amount}` : ""}
+        </span>
+        <span className={styles.link}>{data.link}</span>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.percentage}>
+          {percentage} %
+          <KeyboardArrowUpIcon />
+        </div>
+        {data.icon}
+        {/* <PersonOutlineOutlinedIcon className={styles.icon} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
