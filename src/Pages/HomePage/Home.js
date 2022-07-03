7d44c8006d11b14b617d7436318411a8fe781e5b
Mwau stephen <stephenmwau41@gmail.com>
2022-07-03 16:17:35 +0800

    imported and used the widget component in the home page

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../../Components/navBar/NavBar";
 import SideBar from "../../Components/sideBar/SideBar";
+import Widgets from "../../Components/Widgets/Widgets";
 import styles from "./Home.module.css";
 
 const Home = () => {
@@ -9,7 +10,12 @@ const Home = () => {
       <SideBar />
       <div className={styles["home-container"]}>
         <NavBar />
-        home content
+        <div className={styles.widgets}>
+        <Widgets/>
+        <Widgets/>
+        <Widgets/>
+        <Widgets/>
+        </div>
       </div>
     </div>
   );
