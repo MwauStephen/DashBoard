9489371a68bf6fc873950d6a8d331b5f90e1e3fb
Mwau stephen <stephenmwau41@gmail.com>
2022-07-05 14:50:12 +0800

    started the list section

--- src/Pages/ListPage/List.js
+++ src/Pages/ListPage/List.js
@@ -1,7 +1,18 @@
 import React from "react";
+import NavBar from "../../Components/navBar/NavBar";
+import SideBar from "../../Components/sideBar/SideBar";
+import styles from "./List.module.css";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className={styles.list}>
+      <SideBar />
+      <div className={styles["list-container"]}>
+        <NavBar />
+        dataTable
+      </div>
+    </div>
+  );
 };
 
 export default List;
