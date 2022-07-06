d678ff67457b69a6d3463bb70440a409734297c4
Mwau stephen <stephenmwau41@gmail.com>
2022-07-06 16:06:06 +0800

    started the new page component

--- src/Pages/NewPage/NewPage.js
+++ src/Pages/NewPage/NewPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import NavBar from "../../Components/navBar/NavBar";
+import SideBar from "../../Components/sideBar/SideBar";
+import styles from "./NewPage.module.css";
 
 const NewPage = () => {
-  return <div>NewPage</div>;
+  return (
+    <div className={styles.new}>
+      <SideBar />
+      <div className={styles["new-page"]}>
+        <NavBar />
+      </div>
+    </div>
+  );
 };
 
 export default NewPage;
