495919e3d790c17f8ef9e27e9bd8c63235448921
Mwau stephen <stephenmwau41@gmail.com>
2022-07-06 13:01:03 +0800

    started the single page component

--- src/Pages/SinglePage/SinglePage.js
+++ src/Pages/SinglePage/SinglePage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import NavBar from "../../Components/navBar/NavBar";
+import SideBar from "../../Components/sideBar/SideBar";
+import styles from "./SinglePage.module.css";
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className={styles["single-page"]}>
+      <SideBar />
+      <div className={styles["single-container"]}>
+        <NavBar />
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
