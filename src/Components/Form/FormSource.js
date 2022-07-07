7d298a9310bf64a1a7d868e059fdab6fc674cfad
Mwau stephen <stephenmwau41@gmail.com>
2022-07-08 06:47:16 +0800

    added form data

---
+++ src/Components/Form/FormSource.js
@@ -0,0 +1,76 @@
+export const UserInputs = [
+  {
+    id: 1,
+    label: "UserName:",
+    type: "text",
+    placeholder: "username..",
+  },
+  {
+    id: 2,
+    label: "Name and SurName:",
+    type: "text",
+    placeholder: "name and surname....",
+  },
+  {
+    id: 3,
+    label: "Phone:",
+    type: "number",
+    placeholder: "071234567..",
+  },
+  {
+    id: 4,
+    label: "mail:",
+    type: "text",
+    placeholder: "mail@gmail.com..",
+  },
+  {
+    id: 5,
+    label: "password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "your address..",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "your country..",
+  },
+];
+
+export const ProductInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple watch",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "number",
+    placeholder: "1000",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
