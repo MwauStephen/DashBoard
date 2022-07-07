99591717f7688f34b8ace0443bc52f64a13a37fb
Mwau stephen <stephenmwau41@gmail.com>
2022-07-08 06:47:35 +0800

    added the product route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from "./Pages/LoginPage/Login";
 import List from "./Pages/ListPage/List";
 import SinglePage from "./Pages/SinglePage/SinglePage";
 import NewPage from "./Pages/NewPage/NewPage";
+import { ProductInputs, UserInputs } from "./Components/Form/FormSource";
 function App() {
   return (
     <div>
@@ -14,14 +15,24 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
-            <Route path="list">
+            
+            <Route path="list"> 
               <Route index element={<List />} />
               <Route path=":ListUsers" element={<SinglePage />} />
+              <Route
+                path="new"
+                element={<NewPage inputs={UserInputs} title="Add new user" />}
+              />
             </Route>
-            <Route>
+            <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<SinglePage />} />
-              <Route path="new" element={<NewPage />} />
+              <Route
+                path="new"
+                element={
+                  <NewPage inputs={ProductInputs} title="Add new product" />
+                }
+              />
             </Route>
           </Route>
         </Routes>
