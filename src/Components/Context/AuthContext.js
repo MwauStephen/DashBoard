d2ae00b1fd18f36a0d1310bdf9e6297724fae413
Mwau stephen <stephenmwau41@gmail.com>
2022-07-20 14:36:54 +0800

    re created the authentication context

---
+++ src/Components/Context/AuthContext.js
@@ -0,0 +1,68 @@
+import React, { useReducer } from "react";
+
+//3.intial state
+const initialUserState = {
+  currentUser: null,
+};
+
+// 4.redcuer function
+const userReducerFn = (state, action) => {
+  if (action.type === "LOGIN") {
+    return {
+      currentUser: action.payload,
+    };
+  } else if (action.type === "LOGOUT") {
+    return { currentUser: null };
+  }
+  return {
+    currentUser: state,
+  };
+};
+
+// 1.creating the context
+const AuthContext = React.createContext(initialUserState);
+
+//2. creating and exporting the context provider
+export const AuthContextProvider = (props) => {
+  const [stateUser, dispatchUser] = useReducer(userReducerFn, initialUserState);
+
+  return (
+    <AuthContext.Provider
+      value={{ currentUser: stateUser.currentUser, dispatchUser }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
+
+// import React, { useReducer } from "react";
+
+// const INITIAL_STATE = {
+//   currentUser: null,
+// };
+
+// const userReducerFn = (state, action) => {
+//   if (action.type === "LOGIN") {
+//     return { currentUser: action.payload };
+//   } else {
+//     return { currentUser: null };
+//   }
+
+//   return state;
+// };
+
+// const AuthContext = React.createContext({ INITIAL_STATE });
+
+// export const AuthContextProvider = () => {
+//   const [user, dispatchUser] = useReducer(userReducerFn, INITIAL_STATE);
+
+//   return (
+//     <AuthContext.Provider
+//       value={{ currentUser: user.currentUser, dispatchUser }}
+//     ></AuthContext.Provider>
+//   );
+// };
+
+// export default AuthContext;
