ccc06509c730a2f815421641070e9a7ef0dcbd24
Mwau stephen <stephenmwau41@gmail.com>
2022-07-20 14:37:56 +0800

    wrapped the main component with a provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DarkModeProvider } from "./Components/Context/ModeContext";
+import { AuthContextProvider } from "./Components/Context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <DarkModeProvider>
+      <AuthContextProvider>
       <App />
+      </AuthContextProvider>
     </DarkModeProvider>
   </React.StrictMode>
 );
