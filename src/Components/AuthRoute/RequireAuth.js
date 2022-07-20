383d5da470e21f377f277eb2a4dd77acebba8898
Mwau stephen <stephenmwau41@gmail.com>
2022-07-20 15:01:44 +0800

    finished the first section for login in the user for my dashoard application

--- src/Components/AuthRoute/RequireAuth.js
+++ src/Components/AuthRoute/RequireAuth.js
@@ -4,13 +4,15 @@ import AuthContext from "../Context/AuthContext";
 
 const RequireAuth = (props) => {
   // protecting routes
-  const { currentUser } = useContext(AuthContext);
+  // const { currentUser } = useContext(AuthContext);
+  const ctx = useContext(AuthContext);
   // const currentUser = false;
   // console.log(currentUser);
 
   return (
     <Fragment>
-      {currentUser ? props.children : <Navigate to="/login" />}
+      {/* {currentUser ? props.children : <Navigate to="/login" />} */}
+      {ctx.currentUser ? props.children : <Navigate to="/login" />}
     </Fragment>
   );
 };
