aafdefd84e8ec63830e4164f0e69e03ad7ae0eb2
Mwau stephen <stephenmwau41@gmail.com>
2022-07-22 16:00:32 +0800

    added the database

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   // apiKey: process.env.REACT_APP_KEY,
@@ -13,6 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 // console.log(app);
 
 export const auth = getAuth();
