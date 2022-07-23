ab7385045cef90a27a023fb4bb09bf0a45060f28
Mwau stephen <stephenmwau41@gmail.com>
2022-07-23 12:43:10 +0800

    successfully uploaded an image to the firebase storage

--- src/Pages/NewPage/NewPage.js
+++ src/Pages/NewPage/NewPage.js
@@ -23,9 +23,41 @@ const NewPage = (props) => {
   // upload an image to firebase storage
   useEffect(() => {
     const uploadFile = () => {
-      const name = new Date().getTime() + file.name;
+      const name = new Date().getTime() + file.name; //generates image uniquely.
       console.log(name);
       const storageRef = ref(storage, file.name);
+
+      // actual uploading process
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prevData) => {
+              return {
+                ...prevData,
+                img: downloadURL,
+              };
+            });
+          });
+        }
+      );
     };
     file && uploadFile();
   }, [file]);
