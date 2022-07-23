2269af36b9b96f6d65f1cfd371147bbb16db0a80
Mwau stephen <stephenmwau41@gmail.com>
2022-07-23 13:43:36 +0800

    fetched the data from the database

--- src/Components/datatable/DataTable.js
+++ src/Components/datatable/DataTable.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Box } from "@mui/system";
 import { DataGrid } from "@mui/x-data-grid";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 import { userRows, userColumns } from "../../sourceData";
 
 import styles from "./DataTable.module.css";
 
 const DataTable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
 
-  // const deleteItemRow = (id) => {
-  //   setData(
-  //     data.filter((item) => {
-  //       return item.id !== id;
-  //     })
-  //   );
-  // };
+  useEffect(() => {
+    const fetchData = async () => {
+      const list = [];
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setData(list);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  // console.log(data);
 
   const handleDelete = (id) => {
     setData(
